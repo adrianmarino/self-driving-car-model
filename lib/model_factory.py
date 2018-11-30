cbcbc6e6d262ef062f9f111e71861b69e2a873d9
adrianmarino <adrianmarino@gmail.com>
2018-11-30 11:16:01 -0300

    create a model class, imporve notebook format, content by secion and add a reference section

--- lib/model_factory.py
+++ lib/model_factory.py
@@ -8,12 +8,12 @@ class ModelFactory:
     @staticmethod
     def create_nvidia_model(
             input_shape=(160, 320, 3),
-            normalization_block=lambda x: x / 127.5 - 1.0,
-            dropout_rate=0.5,
+            input_normalization=lambda x: x / 127.5 - 1.0,
+            cnn_end_dropout_rate=0.5,
             activation='elu'
     ):
         model = Sequential()
-        model.add(Lambda(normalization_block, input_shape=input_shape))
+        model.add(Lambda(input_normalization, input_shape=input_shape))
 
         model.add(Conv2D(filters=24, kernel_size=(5, 5), strides=(2, 2), activation=activation))
         model.add(Conv2D(filters=36, kernel_size=(5, 5), strides=(2, 2), activation=activation))
@@ -22,7 +22,7 @@ class ModelFactory:
         model.add(Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation=activation))
         model.add(Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), activation=activation))
 
-        model.add(Dropout(dropout_rate))
+        model.add(Dropout(cnn_end_dropout_rate))
 
         model.add(Flatten())
 
